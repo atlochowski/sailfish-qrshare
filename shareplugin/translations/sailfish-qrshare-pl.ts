0c7817d024cfc8e8c5a7d5ad95192f6c1435e509
A <hucul@o2.pl>
2019-05-15 18:53:04 +0200

    Create sailfish-qrshare-pl.ts
    Polish translation

---
+++ shareplugin/translations/sailfish-qrshare-pl.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="qrshare-display_name">
+        <source>QR Share</source>
+        <extracomment>Plugin display name</extracomment>
+        <translation>Generuj kod QR</translation>
+    </message>
+    <message id="qrshare-menu-save_to_gallery">
+        <source>Save to Gallery</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Zapisz do Galerii</translation>
+    </message>
+    <message id="qrshare-placeholder-text_too_long">
+        <source>Text is too long for QR code.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Tekst jest za długi dla kodu QR.</translation>
+    </message>
+    <message id="qrshare-placeholder-no_text">
+        <source>Nothing to share.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Brak rzeczy do udostępnienia.</translation>
+    </message>
+</context>
+</TS>
